eb1c7ed91ddf601ccc6d2cd04c9c1e0cd485c957
gcot970 <127463356+gcot970@users.noreply.github.com>
2023-05-25 23:17:55 -0500

    Added classes to the project in favor of module, updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# SVG-Logo-Maker
+# SVG Logo Maker
+
+## Description
+
+This is a simple logo maker that creates different logos based off of user prompts. This is so each project that I make can always have a logo. This is done by utilizing classes to render the SVG contents to a file. With that, I've learned more troubleshooting techniques while trying to get my classes to cooperate with a base class-less code I've made previously.
+
+
+## Installation
+
+This requires the Inquirer package from NPM. Run npm start to get the environment up and running.
+
+## Usage
+
+Run 'node index.js' then answer the questions of each prompt, and it will provide you with examples like these:
+![A red circle with white text that says App](examples/applogo.svg)
+![A blue triangle with black text that says EAE](examples/eaelogo.svg)
+![A green circle with white text that says SVG](examples/logo.svg)
+![A grey box with black text that says Win](examples/Winlogo.svg)
+![An animation of the program running](examples/example.gif)
+
+
--- index.js
+++ index.js
@@ -1,48 +1,73 @@
 const fs = require('fs');
-const genSVG = require('./utils/generateSVG');
 const inquirer = require('inquirer');
+const { Triangle, Circle, Square } = require('./lib/shapes');
 
-function writeToFile(fileName, data) {
-    fs.appendFile(fileName, `${data}\n`, (err) =>
-        err ? console.error(err) : console.log('Generated logo.svg')
-    );
-}
+// LogoGenerator class
+class LogoGenerator {
+    constructor() {
+        // initializing shapes
+        this.shapes = {
+            Triangle: new Triangle(),
+            Circle: new Circle(),
+            Square: new Square(),
+        };
+    }
 
-function init() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'text',
-                message: 'Enter three characters:',
-                validate: function (input) {
-                    if (input.length === 3) {
-                        return true;
-                    } else {
-                        return 'Please enter exactly three characters.';
-                    }
+    // writeToFile
+    writeToFile(fileName, data) {
+        fs.appendFile(fileName, `${data}\n`, (err) =>
+            err ? console.error(err) : console.log('Generated logo.svg')
+        );
+    }
+
+    // The "main code"
+    init() {
+        // Asking user prompts
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'text',
+                    message: 'Enter three characters:',
+                    validate: function (input) {
+                        if (input.length === 3) {
+                            return true;
+                        } else {
+                            return 'Please enter exactly three characters.';
+                        }
+                    },
                 },
-            },
-            {
-                type: 'input',
-                message: 'Enter a color (hex or name) for text:',
-                name: 'textColor',
-            },
-            {
-                type: 'list',
-                name: 'shape',
-                message: 'Select an option:',
-                choices: ['Circle', 'Triangle', 'Square'],
-            },
-            {
-                type: 'input',
-                message: 'Enter a color (hex or name) for shape:',
-                name: 'shapeColor',
-            },
-        ])
-        .then((data) => {
-            writeToFile('logo.svg', genSVG(data));
-        });
+                {
+                    type: 'input',
+                    message: 'Enter a color (hex or name) for text:',
+                    name: 'textColor',
+                },
+                {
+                    type: 'list',
+                    name: 'shape',
+                    message: 'Select an option:',
+                    choices: ['Circle', 'Triangle', 'Square'],
+                },
+                {
+                    type: 'input',
+                    message: 'Enter a color (hex or name) for shape:',
+                    name: 'shapeColor',
+                },
+            ])
+            .then((data) => {
+                // Creates new shape
+                const shape = this.shapes[data.shape];
+                // Sets shape color
+                shape.setColor(data.shapeColor);
+                // "Renders" text over the shape
+                const svgContent = shape.render(data.text, data.textColor);
+                // Begins writing to the file
+                this.writeToFile('logo.svg', svgContent);
+            });
+    }
 }
 
-init();
+// Creates a new instance of LogoGenerator
+const logoGenerator = new LogoGenerator();
+// Starts the "main" code
+logoGenerator.init();
